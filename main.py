4165e171d345262085a5dc7580057b2e6e79df14
unknown <B00156016@mytudublin.ie>
2026-03-03 20:32:04 +0000

    CCL finally fully implemented and all smaller components not considered

--- main.py
+++ main.py
@@ -1,7 +1,62 @@
+#KNOWLEDGE - Why do we convert the image from Grayscale to Binary?
+    # Because we are ONLY concerned with whether the O-Rings are normal or defective,
+    #  making this is a BINARY problem.
+    #   We do loose information by thresholding but we maintain ONLY information we need
+    #    for this task
+
+    #KNOWLEDGE - OTSU's THRESHOLDING APPROACH
+    # FORMULA: w0 ​* w1​(μ0 ​− μ1​)²
+    # w0 - % of Class 0
+    #  w1 - % of Class 1
+    #   μ0 - Mean of Class 0
+    #    μ1​ - Mean of Class 1
+    # 1) We check every possible threshold value (E.g. threshold = 200) from 0 to 255 and for every threshold we split
+    #       the pixels into TWO classes. One class stores the pixels that have an intensity HIGHER than the threshold. The
+    #       other class stores the pixels with intensity LOWER than the threshold value.
+    # 2a) Next, we calculate the mean intensity of each class. This means we look at the intensity value of each pixel in the LOWER class,
+    #        and calculate the mean of them. We do the same for the pixels in the HIGHER than threshold class.
+    #        We repeat this process for every threshold value, meaning we have (255 x 2) MEAN values.
+    # 2b) Afterwards, we also calculate the probability of each class, so the proportion of pixels that are HIGHER and LOWER than the threshold.
+    # 3) Next, we comparse the mean intensity's we got from Part 2a. IF, the mean values of the HIGHER class is far apart from the LOWER class, then
+    #       that means we have found a good value that SEPARATES our foreground from background pixels. If the mean intensity's are close, then that
+    #       means that the threshold values does not SEPARATE our classes well.
+    #       NOTE: Otsu's Method also considers the PROPORTION of pixels from Part 2b. This means a threshold value that separates the mean intensity's well
+    #           but has a LARGE class imbalance is not chosen.
+    #           NOTE: Otsu does NOT determine a good class balance if they are EQUALLY-SIZED (E.g. 50% of pixels BELOW, 50% of pixels ABOVE), but rather any split AS LONG AS
+    #               there isn't any class that is almost empty or too full.
+    # 4) Otsu's determines the most optimal threshold according to the classes being meaningfully sized and mean intensities are far apart
+
+    #KNOWLEDGE - Binary Morphology
+    #Binary Morphology is an image processing technique that usually acts on the foreground.
+    # That is why we will flip the Foreground and Background classes (I.e. Our rings will be white and our background will be black) 
+    # --------------------------
+    # Types of Binary Morphology
+    # --------------------------
+    # EROSION: Used to remove noise/ small blobs
+    # Step 1) Looks at its 3x3 neighbourhood
+    # Step 2) If ALL pixels in that window are WHITE, 'current pixel' = WHITE
+    # Step 3) If even one pixel is BLACK, 'current pixel = BLACK'
+    
+    # DILATION: Used to fill in small gaps or holes
+    # Step 1) Looks at its 3x3 neighbourhood
+    # Step 2) If ANY pixels in that window are WHITE, 'current pixel' = WHITE
+    # Step 3) If even one pixel is BLACK, 'current pixel = BLACK'
+      
+    # 3) OPENING = EROSION + DILATION - Used to remove ISOLATED WHITE noise/ pixels 
+    # 4) CLOSING = DILATION + EROSION - Used to fill small holes inside 
+
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+
+#STEP 1    
+def imageHistogram(img):
+    histogram = np.zeros(256)
+    for x in range (img.shape[0]):
+        for y in range (img.shape[1]):
+            histogram[img[x, y]] += 1
+    return histogram
 
 #STEP 2
 def threshold(img, thresholdValue):
@@ -12,14 +67,6 @@ def threshold(img, thresholdValue):
             else:
                 img[x,y] = 0
     return img
-
-#STEP 1    
-def imageHistogram(img):
-    histogram = np.zeros(256)
-    for x in range (img.shape[0]):
-        for y in range (img.shape[1]):
-            histogram[img[x, y]] += 1
-    return histogram
 
 #STEP 2
 def calculateThreshold(img):
@@ -82,6 +129,40 @@ def dilation(img, k = 1):
                 dilatedImage[x, y] = 0
     return dilatedImage
 
+#STEP 4
+def ccl8Neighbours(img):
+    imageHeight, imageWidth = img.shape
+    imageLabels = np.zeros_like(img, dtype = int)
+    currentLabel = 1
+
+    #Defining our 8-Connected Neighbour Offsets
+    neighbours = [(-1, -1), (-1, 0), (-1, 1),
+                  (0, -1),           (0, 1),
+                  (1, -1),  (1, 0),  (1, 1)]
+    
+    for x in range(imageHeight):
+        for y in range(imageWidth):
+            #We start off by checking if we are looking at a pixel 
+            # that is foreground that has not been given a label yet
+            if (img[x, y] == 255 and imageLabels[x, y] == 0):
+                #If it hasn't we will assign it a new component object
+                objectStack = [(x, y)]
+                imageLabels[x, y] = currentLabel
+
+                while objectStack:
+                    cx, cy = objectStack.pop()
+                    for dx, dy in neighbours:
+                        nx, ny = cx + dx, cy + dy
+                        if 0 <= nx < imageHeight and 0 <= imageWidth:
+                            if img[nx, ny] == 255 and imageLabels[nx, ny] == 0:
+                                imageLabels[nx, ny] = currentLabel
+                                objectStack.append((nx, ny))
+                currentLabel += 1
+
+    return imageLabels, currentLabel - 1
+
+
+
 for i in range(1, 16):
     #We use the '.imread()' function in the OpenCV library to read in all our images into memory
     # NOTE: Since our images are grayscale and we want to load them that way, set add an extra argument at the end  '0'.
@@ -112,7 +193,22 @@ for i in range(1, 16):
     #     NOTE: Once labeled, we can then also calculate properties such as:
     #     - Area, Perimeter, Bounding Box, Circularity etc.
     #      The CORE idea behind CCL is that we STRUCTURE the image into seperate OBJECTS
-    #STEP 1
+    #   
+    # 4 OR 8-CONNECTED CL
+    #To identify components (I.e. Rubber parts, the actual O-Ring etc.) we use either 4 or 8 Connection
+    #-----------------------------------------------------------------------------------------
+    # EXAMPLE SECTION from an image: (Pixels are going to be BACKGROUND (0) or FOREGROUND (1))
+    # 0 1 0 0 1 0
+    # 1 1 0 1 1 0
+    # 0 0 1 0 0 1
+    # 1 0 1 1 0 0
+    # 0 1 0 0 1 1
+    # 0 0 1 0 1 0
+    #-----------------------------------------------------------------------------------------
+    # - 4-CONNECTION: We look at our N, S, W, E and we find there are 8 COMPONENTS
+    # - 8-CONNECTION: We look at our N, NW, NE, S, SW, SE, W, E and we find there is 1 HUGE COMPONENT
+    #NOTE: Pixels that touch diagonally or horizontally become part of the SAME component!
+    #NOTE 2: We will use 8-CONNECTION as our O-Rings are circular, THEREFORE, we are going to be using diagonals a lot. 
     pixelIntensityHistogram = imageHistogram(img)
     plt.figure(figsize = (8, 4))
     plt.scatter(range(256), pixelIntensityHistogram, color = 'grey', s = 10)
@@ -120,54 +216,6 @@ for i in range(1, 16):
     plt.xlabel("Pixel Intensity {0 = BLACK, 255 = WHITE}")
     plt.ylabel("Number of Pixels")
     plt.show()
-
-    #KNOWLEDGE - Why do we convert the image from Grayscale to Binary?
-    # Because we are ONLY concerned with whether the O-Rings are normal or defective,
-    #  making this is a BINARY problem.
-    #   We do loose information by thresholding but we maintain ONLY information we need
-    #    for this task
-
-    #KNOWLEDGE - OTSU's THRESHOLDING APPROACH
-    # FORMULA: w0 ​* w1​(μ0 ​− μ1​)²
-    # w0 - % of Class 0
-    #  w1 - % of Class 1
-    #   μ0 - Mean of Class 0
-    #    μ1​ - Mean of Class 1
-    # 1) We check every possible threshold value (E.g. threshold = 200) from 0 to 255 and for every threshold we split
-    #       the pixels into TWO classes. One class stores the pixels that have an intensity HIGHER than the threshold. The
-    #       other class stores the pixels with intensity LOWER than the threshold value.
-    # 2a) Next, we calculate the mean intensity of each class. This means we look at the intensity value of each pixel in the LOWER class,
-    #        and calculate the mean of them. We do the same for the pixels in the HIGHER than threshold class.
-    #        We repeat this process for every threshold value, meaning we have (255 x 2) MEAN values.
-    # 2b) Afterwards, we also calculate the probability of each class, so the proportion of pixels that are HIGHER and LOWER than the threshold.
-    # 3) Next, we comparse the mean intensity's we got from Part 2a. IF, the mean values of the HIGHER class is far apart from the LOWER class, then
-    #       that means we have found a good value that SEPARATES our foreground from background pixels. If the mean intensity's are close, then that
-    #       means that the threshold values does not SEPARATE our classes well.
-    #       NOTE: Otsu's Method also considers the PROPORTION of pixels from Part 2b. This means a threshold value that separates the mean intensity's well
-    #           but has a LARGE class imbalance is not chosen.
-    #           NOTE: Otsu does NOT determine a good class balance if they are EQUALLY-SIZED (E.g. 50% of pixels BELOW, 50% of pixels ABOVE), but rather any split AS LONG AS
-    #               there isn't any class that is almost empty or too full.
-    # 4) Otsu's determines the most optimal threshold according to the classes being meaningfully sized and mean intensities are far apart
-
-    #KNOWLEDGE - Binary Morphology
-    #Binary Morphology is an image processing technique that usually acts on the foreground.
-    # That is why we will flip the Foreground and Background classes (I.e. Our rings will be white and our background will be black) 
-    # --------------------------
-    # Types of Binary Morphology
-    # --------------------------
-    # EROSION: Used to remove noise/ small blobs
-    # Step 1) Looks at its 3x3 neighbourhood
-    # Step 2) If ALL pixels in that window are WHITE, 'current pixel' = WHITE
-    # Step 3) If even one pixel is BLACK, 'current pixel = BLACK'
-    
-    # DILATION: Used to fill in small gaps or holes
-    # Step 1) Looks at its 3x3 neighbourhood
-    # Step 2) If ANY pixels in that window are WHITE, 'current pixel' = WHITE
-    # Step 3) If even one pixel is BLACK, 'current pixel = BLACK'
-      
-    # 3) OPENING = EROSION + DILATION - Used to remove ISOLATED WHITE noise/ pixels 
-    # 4) CLOSING = DILATION + EROSION - Used to fill small holes inside 
-    
 
     thresholdValue = calculateThreshold(img)
     print("Optimal Threshold Chosen: ", thresholdValue)
@@ -178,29 +226,47 @@ for i in range(1, 16):
 
     #ERODED Image
     erodedImage = erosion(bw, k = 1)
+
     #DILATED Image
-    dilatedImage = dilation(bw, k = 2)
+    dilatedImage = dilation(bw, k = 1)
 
-    plt.figure(figsize = (12, 4))
+    #CLEANED Image
+    imageLabels, numberOfLabels = ccl8Neighbours(dilatedImage)
+    print("Number of Components Detected: ", numberOfLabels)
 
+    #Here we keep only the larget component, being the O-Ring
+    imageAreas = [np.sum(imageLabels == i)
+                  for x in range(1, numberOfLabels + 1)
+                  ]
+    largestLabel = np.argmax(imageAreas) + 1 
+
+    cleanedImage = np.zeros_like(imageLabels, dtype = np.uint8)
+    cleanedImage[imageLabels == largestLabel] = 255
+
+    plt.figure(figsize = (16, 5))
     #Displaying the Original image
-    plt.subplot(1, 3, 1)
+    plt.subplot(1, 4, 1)
     plt.title(f"Original O-Ring {i}")
     plt.imshow(bw, cmap = 'gray')
     plt.axis("off")
 
     #Displaying the image after its been eroded
-    plt.subplot(1, 3, 2)
+    plt.subplot(1, 4, 2)
     plt.title(f"Eroded O-Ring {i}")
     plt.imshow(erodedImage, cmap = 'gray')
     plt.axis("off")
 
     #Displaying the eroded image after its been dilated
-    plt.subplot(1, 3, 3)
+    plt.subplot(1, 4, 3)
     plt.title(f"Dilated O-Ring {i}")
     plt.imshow(dilatedImage, cmap = 'gray')
     plt.axis("off")
 
+    #Displaying the Cleaned image after its been CCL'd
+    plt.subplot(1, 4, 4)
+    plt.title(f"Cleaned O-Ring {i}")
+    plt.imshow(cleanedImage, cmap = 'gray')
+    plt.axis("off")
     #Display the figure that contains all images
     plt.show()
 
